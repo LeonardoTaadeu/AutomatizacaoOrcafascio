f1b904bd06219e993654a719812cf4aeec3b0d79
Leonardo <leonardo.pedrobr@gmail.com>
2025-10-20 18:17:42 -0300

    20202

--- cypress/e2e/testSpec.cy.js
+++ cypress/e2e/testSpec.cy.js
@@ -5,15 +5,40 @@ describe('Teste de códigos do Cypress', () => {
         beforeEach(() => {
             cy.sessionLogin()
             cy.visit('/home')
-            cy.viewport(1280, 720) //Configuração do tamanho da tela
+            cy.viewport(1280, 720) //Configuração do tamanho da tela //
         })
         Cypress.on('uncaught:exception', (err, runnable) => {
          return false
          })
 
+         it('Criando um novo orcamento', () => {
+            cy.get('.sh-logopanel').click()
+            cy.get(':nth-child(2) > .with-sub').click()
+            cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
+            cy.get('[href="/orc/orcamentos/new"] > div').click()
+            cy.get(':nth-child(3) > .radio-label > [name="orc_orcamento[version_2023]"]').click()
+            cy.get('[name="orc_orcamento[codigo]"]').clear()
+            cy.get('[name="orc_orcamento[codigo]"]').type('Teste Orçamento 123456789 Automatizado')
+            cy.get('[name="orc_orcamento[descricao]"]').type('Teste Orçamento 123456789 Automatizado')
+            cy.get('[name="orc_orcamento[cliente_id]"]').select('Leonardo')
+            cy.get('[name="orc_orcamento[standard_category_name]"]').select('Outros')
+            cy.get('[name="orc_orcamento[custom_category_name]"]').type('Teste Automatizado')
+            cy.get('[name="orc_orcamento[validity]"]').type('2026-07-20T08:20')
+            cy.get('#orc_orcamento_insumos_zerados').click()
+            cy.get('[name="commit"]').click()
+            cy.get('[name="commit"]').click()
+
+            const nomeBancos = ['SBC', 'SICRO3', 'SIURB', 'SIURB INFRA', 'FDE', 'CPOS', 'EMOP', 'SCO', 'SETOP', 'SUDECAP', 'IOPES', 'ORSE', 'SEINFRA', 'CAEMA', 'EMBASA', 'CAERN', 'COMPESA', 'AGESUL', 'AGETOP CIVIL', 'AGETOP RODOVIARIA', 'SEDOP', 'DERPR']
+            cy.wrap(nomeBancos).each((selecionandoBanco) => {
+                cy.get(`[name="${selecionandoBanco}_exibir_relatorio"]`).check({force: true})
+            })
+
+            cy.get(':nth-child(55) > .col > .right > [name="commit"]').click()
+         })
+
          it('Criando Etapas de um orçamento', () => {
 
-            const nomeEtapas = ['SERVIÇOS PRELIMINARES', 'FUNDAÇÕES', 'PAV-1', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-2', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-3', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-4', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-5', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-6', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-7', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-8', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-9', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-10', 'PISOS', 'PAREDES', 'ESQUADRIAS']
+            const nomeEtapas = ['SERVIÇOS PRELIMINARES', 'FUNDAÇÕES', 'PAV-1', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-2', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-3', 'PISOS', 'PAREDES', 'ESQUADRIAS', 'PAV-4']
 
             cy.get('.sh-logopanel').click()
             cy.get(':nth-child(2) > .with-sub').click()
@@ -36,7 +61,7 @@ describe('Teste de códigos do Cypress', () => {
             cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
             cy.get('#filtro_text').type('Teste')
             cy.contains('Teste Orçamento').click()
-            cy.wait(1000)
+            cy.wait(2000)
 
                 cy.wrap(dadosSinapi).each((item) => {
                     cy.get(':nth-child(2) > .dropdown > .add_item_end').click()
@@ -52,7 +77,12 @@ describe('Teste de códigos do Cypress', () => {
 
 
         it('Aplicação de BDI', () =>{
-            cy.visit('orc/orcamentos/68af5bbecab727f01b964938')
+            cy.get('.sh-logopanel').click()
+            cy.get(':nth-child(2) > .with-sub').click()
+            cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
+            cy.get('#filtro_text').type('Teste')
+            cy.contains('Teste Orçamento').click()
+            cy.wait(1000)   
             cy.get('#budget_config_bdi > th.tx-right > a').click()
             cy.wait(1000)
             cy.get('#bdi_manual').click()
@@ -63,13 +93,18 @@ describe('Teste de códigos do Cypress', () => {
         })
 
         it('Retirando BDI', () => {
-            cy.visit('orc/orcamentos/68af5bbecab727f01b964938')
+            cy.get('.sh-logopanel').click()
+            cy.get(':nth-child(2) > .with-sub').click()
+            cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
+            cy.get('#filtro_text').type('Teste')
+            cy.contains('Teste Orçamento').click()
+            cy.wait(1000)
             cy.get('#budget_config_bdi > th.tx-right > a').click()
             cy.get('#\\30 ').clear()
             cy.get('.form-bdi > .modal-content > .modal-footer > .btn-primary').click()
         })
 
-        it('Apagando o orçamento', () => {
+        it('Apagando os itens do orçamento', () => {
             cy.get('.sh-logopanel').click()
             cy.get(':nth-child(2) > .with-sub').click()
             cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
@@ -80,5 +115,19 @@ describe('Teste de códigos do Cypress', () => {
             cy.contains('a', '103155').rightclick()
             cy.get('.items_delete_all > .bg-resource').click()
         })
+
+        it('Apagando o orçamento inteiro', () => {
+            cy.get('.sh-logopanel').click()
+            cy.get(':nth-child(2) > .with-sub').click()
+            cy.get(':nth-child(2) > .nav-sub > :nth-child(1) > .nav-link').click()
+            cy.get('#filtro_text').type('Teste')
+            cy.contains('Teste Orçamento').click()
+            cy.wait(1000)
+            cy.get(':nth-child(2) > #navbarDropdown').click({force: true})
+            cy.get('[data-target="#modal-budget-exclusion"]').click()
+            cy.wait(1000)
+            cy.get('[name="texto_de_exclusao"]').type('excluir')
+            cy.get('#modal-budget-exclusion > .modal-dialog > .modal-content > .modal-footer > .btn').click()
+         })
     })
 })
